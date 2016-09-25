2fc11f3356fbfab6c22ff677275c7b63f5883709
stevetu <stevetu717@outlook.com>
2016-09-25 05:18:24 -0400

    Initial commit. Working registration and login screen. No hash for master password in shared preferences - compares plaintext.

---
+++ app/src/main/java/com/steve/strongpass/AccountList.java
@@ -0,0 +1,13 @@
+package com.steve.strongpass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_list);
+    }
+}
